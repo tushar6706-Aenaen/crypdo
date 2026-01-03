ccc7bd54050b4fb2a5ac351d3f728f44099d7b8c
.env <chauhantushar6706@gmail.com>
2026-01-04 03:14:22 +0530

    coderabbit bugs fixed

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -4,27 +4,28 @@ import Image from 'next/image'
 import React from 'react'
 
 const CoinOverview = async () => {
+    let coin;
     try {
-        const coin = await fetcher<CoinDetailsData>('/coins/bitcoin', {
+         coin = await fetcher<CoinDetailsData>('/coins/bitcoin', {
             dex_pair_format: 'symbol',
         })
-
-        return (
-            <div id="coin-overview">
-                <div className="header pt-2">
-                    <Image src={coin.image.large} alt={coin.name} width={56} height={56} />
-                    <div className="info">
-                        <p>
-                            {coin.name} / {coin.symbol.toUpperCase()}
-                        </p>
-                        <h1>{formatCurrency(coin.market_data.current_price.usd)} </h1>
-                    </div>
-                </div>
-            </div>
-        )
     } catch (error) {
         return <div>Error loading coin data</div>
     }
+    return (
+        <div id="coin-overview">
+            <div className="header pt-2">
+                <Image src={coin.image.large} alt={coin.name} width={56} height={56} />
+                <div className="info">
+                    <p>
+                        {coin.name} / {coin.symbol.toUpperCase()}
+                    </p>
+                    <h1>{formatCurrency(coin.market_data.current_price.usd)} </h1>
+                </div>
+            </div>
+        </div>
+    )
+
 }
 
 export default CoinOverview
